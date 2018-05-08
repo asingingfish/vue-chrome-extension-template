6b47f3dcc62dcfe03295fdd78ff392152c1e7667
yuyiming <yuyiming@made-in-china.com>
2018-05-08 17:26:02 +0800

    update src

---
+++ template/src/view/popup/root.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>popup</div>
+</template>
+<script>
+    export default {
+        data: () => ({}),
+        computed: {},
+        created() {
+        },
+        mounted() {
+        },
+        methods: {
+            tab() {
+                chrome.tabs.create({url: 'pages/app.html'});
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    div {
+        color: blue
+    }
+</style>
